601da6e8723c2a928d560bfd66b03142f2acc92c
eyadazab1 <eyad.azzab@gmail.com>
2025-11-12 15:12:51 +0200

    Enhance main function with animal management
    Added animal instances to the main function and displayed them.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,32 @@
 #include "Animal.h"
-#include "Bird.h"
 #include "Mammal.h"
+#include "Bird.h"
 #include "Reptile.h"
 #include "Enclosure.h"
 #include "Visitor.h"
-#include <iostream>
-using namespace std;
 
-
-// ============== MAIN FUNCTION ==============
 int main() {
-    
-    
+    Enclosure* enclosure1 = new Enclosure(10);
+
+    Mammal* lion = new Mammal("Lion", 5, true, "Golden");
+    Bird* parrot = new Bird("Parrot", 2, false, 0.25);
+    Reptile* snake = new Reptile("Snake", 3, true, true);
+
+    enclosure1->addAnimal(lion);
+    enclosure1->addAnimal(parrot);
+    enclosure1->addAnimal(snake);
+
+    cout << "Enclosure 1 Animals:" << endl;
+    enclosure1->displayAnimals();
+
+    Visitor visitor("Sarah Ali", 3);
+    cout << "\nVisitor Info:" << endl;
+    visitor.displayInfo();
+
+    delete enclosure1;
+    delete lion;
+    delete parrot;
+    delete snake;
+
     return 0;
 }
